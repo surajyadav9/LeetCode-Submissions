97509a78fd097a2e21b7795a8fb7424d60f5f055
Suraj Yadav <surajyadav90388@gmail.com>
2019-09-29 09:00:19 +0000

    Sync LeetCode submission - Divisor Game (javascript)

---
+++ problems/divisor_game/solution.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} N
+ * @return {boolean}
+ */
+var divisorGame = function(N) {
+    select = N-1;
+    if(select%2 == 0)
+        return false;
+    else
+        return true;
+};
