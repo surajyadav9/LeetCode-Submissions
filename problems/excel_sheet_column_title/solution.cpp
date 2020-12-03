4188461178da53cdd70e0c5ad1d5503ef5d92fa7
Suraj Yadav <surajyadav90388@gmail.com>
2020-12-03 07:24:59 +0000

    Sync LeetCode submission - Excel Sheet Column Title (cpp)

---
+++ problems/excel_sheet_column_title/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string convertToTitle(int n) {
+        string res ="";        
+        while(n>0) {
+            int r = (n-1)%26;
+            res += 'A'+r;
+            n = (n-1)/26;
+        }
+        
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
