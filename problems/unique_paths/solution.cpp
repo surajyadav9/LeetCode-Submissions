fe6b7eb5f7cf4900f87d8d8c75d11af6f582e531
Suraj Yadav <surajyadav90388@gmail.com>
2020-12-11 12:30:07 +0000

    Sync LeetCode submission - Unique Paths (cpp)

--- problems/unique_paths/solution.cpp
+++ problems/unique_paths/solution.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        int grid[m][n];
+        int N = m + n -2;
+        int r = min(m-1, n-1);
         
-        // There is only 1 path from Right-most and Bottom-most column
-        
-        // Assigning 1 to the Right-most column
-        for(int r=0; r<m; r++) grid[r][n-1]=1;
-        
-        // Assigning 1 to the Bottom-most column
-        for(int c=0; c<n; c++) grid[m-1][c]=1;
-        
-        grid[m-1][n-1] = 1;
-        
-        for(int c=n-2; c>=0; c--) {
-            for(int r=m-2; r>=0; r--) {
-                grid[r][c] = grid[r+1][c] + grid[r][c+1];
-            }
+        long long int res = 1LL;
+        for(int i=1; i<=r; i++) {
+            res *= (N-r+i);
+            res /= i;
         }
         
-        return grid[0][0];
+        return res;
+        
+        
     }
 };
